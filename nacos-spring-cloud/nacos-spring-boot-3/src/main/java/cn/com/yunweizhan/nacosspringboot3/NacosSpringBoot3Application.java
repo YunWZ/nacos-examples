674c678c4c924aa3b960661d1faddecb15729b8b
Weizhan Yun <yunweizhan@gmail.com>
2022-12-25 15:19:09 +0800

    add springboot3.0 application which is using nacos3.0(SNAPSHOT).

---
+++ nacos-spring-cloud/nacos-spring-boot-3/src/main/java/cn/com/yunweizhan/nacosspringboot3/NacosSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package cn.com.yunweizhan.nacosspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSpringBoot3Application.class, args);
+	}
+
+}
---
+++ nacos-spring-cloud/nacos-spring-boot-3/src/main/java/cn/com/yunweizhan/nacosspringboot3/controller/HiController.java
@@ -0,0 +1,29 @@
+package cn.com.yunweizhan.nacosspringboot3.controller;
+
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Weizhan▪Yun
+ * @date 2022/12/25 14:15
+ */
+@RestController
+@RequestMapping("/hello")
+public class HiController {
+	private ConfigService configService;
+
+	HiController(ConfigService configService) {
+		this.configService = configService;
+	}
+
+	@GetMapping("/config")
+	public String config() throws NacosException {
+		String config = configService.getConfig("config-example-1", "DEFAULT_GROUP", 10000L);
+		return config;
+	}
+
+}
